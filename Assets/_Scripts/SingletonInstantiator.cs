672d8943fa694a96c6719354012301ef5fbcffd3
greg Klein <Greg.Klein@EF.com>
2018-04-20 22:55:00 -0400

    singleton creator

---
+++ Assets/_Scripts/SingletonInstantiator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonInstantiator : MonoBehaviour {
+    public GameObject[] singletons;
+
+
+	// Use this for initialization
+	void Start () {
+        foreach (var singleton in singletons)
+        {
+            Instantiate(singleton, transform);
+        }
+	}
+}
