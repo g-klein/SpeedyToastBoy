53216548b5aa48534b7a432810eabb257049e011
Jeremy Simms <jeremyrs@gmail.com>
2018-04-20 22:54:42 -0400

    Add health meter sprite.

--- Assets/_Scripts/Environment/JumpPadBehavior.cs
+++ Assets/_Scripts/Environment/JumpPadBehavior.cs
@@ -2,24 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GameObjectTags {
-	public const string Player = "Player";
-	public const string Enemy = "Enemy";
-	public const string Environment = "Environment";
-}
 
 public class JumpPadBehavior : MonoBehaviour {
 	public Vector2 ForceVector = new Vector2(0.0f, 300.0f);
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		var rb = coll.gameObject.GetComponent<Rigidbody2D> ();
