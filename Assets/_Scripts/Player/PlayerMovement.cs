d95a2b8d4415cf17eed11e6f958b332038bd4443
greg Klein <Greg.Klein@EF.com>
2018-04-21 00:54:08 -0400

    player mode controller

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -27,8 +26,6 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-        Debug.Log(grounded);
-
         if (Input.GetButtonDown("Jump") && grounded)
         {
             jump = true;
