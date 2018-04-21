cb189da9e50558e09175e5ac3973d67a28131d86
greg Klein <Greg.Klein@EF.com>
2018-04-20 23:56:15 -0400

    merge master into movement

---
+++ Assets/_Scripts/Player/PlayerInstance.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInstance : MonoBehaviour {
+    public static PlayerInstance Instance;
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+}
